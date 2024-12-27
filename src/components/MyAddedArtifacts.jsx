5fca6a886a3dc7878ea66e5f0aa7f87533c44483
Tanim Tassanim <tanimtassanim235@gmail.com>
2024-12-27 12:27:44 +0600

    delete funcanality done in my added artifacts page

--- src/components/MyAddedArtifacts.jsx
+++ src/components/MyAddedArtifacts.jsx
@@ -5,10 +5,42 @@ import { FaCalendarAlt } from 'react-icons/fa';
 import { FaLocationPin, FaPlaceOfWorship } from 'react-icons/fa6';
 import { FcEditImage } from 'react-icons/fc';
 import { LiaEdit } from 'react-icons/lia';
+import Swal from 'sweetalert2';
 
-const MyAddedArtifacts = ({ singleArt }) => {
-    console.log(singleArt);
-    const { name, image, artifactsType, createdAt, discoveredPlace, discoveredPerson, currentLocation, adderName, adderEmail, context } = singleArt
+const MyAddedArtifacts = ({ singleArt, art, setArt }) => {
+    const { _id, name, image, artifactsType, createdAt, discoveredPlace, discoveredPerson, currentLocation, adderName, adderEmail, context } = singleArt;
+
+    const handleDelete = _id => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:4000/histories/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        const remainingArt = art.filter((singleData) => _id != singleData?._id);
+                        setArt(remainingArt)
+                        if (data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your Added Artifacts has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+        });
+    }
+
+
     return (
         <div>
             {/* card main div */}
@@ -18,26 +50,26 @@ const MyAddedArtifacts = ({ singleArt }) => {
                     <img src={image} className='w-full h-[340px] object-cover rounded-s-xl rounded-ee-btn' alt={name} />
                 </div>
                 {/* other content div */}
-                <div className='text-lg'>
+                <div className='text-lg px-3'>
                     <div className='flex justify-evenly my-4 text-2xl'>
                         <button className=''>
                             <LiaEdit></LiaEdit>
                         </button>
-                        <button>
+                        <button className='hover:scale-110 hover:text-red-500' onClick={() => handleDelete(_id)}>
                             <AiFillDelete></AiFillDelete>
                         </button>
                     </div>
                     <div className='flex items-center gap-3 py-2'>
-                        <FaPlaceOfWorship className='text-blue-300'></FaPlaceOfWorship>
-                        <p className='text-xl'>Current Location:  <span className='font-bold'>{currentLocation}</span></p>
+                        <FaPlaceOfWorship className='text-blue-300 text-xl'></FaPlaceOfWorship>
+                        <p>Current Location:  <span className='font-bold'>{currentLocation}</span></p>
                     </div>
                     <div className='flex items-center gap-3'>
-                        <FaCalendarAlt className='text-blue-300'></FaCalendarAlt>
+                        <FaCalendarAlt className='text-blue-300 text-xl'></FaCalendarAlt>
                         <p>Created Time Period:  <span className='font-bold text-gray-700'>{createdAt}</span></p>
                     </div>
                     <div className='flex items-center gap-3 py-2'>
                         <BiWorld></BiWorld>
-                        <p>Discoverd By:  <span className='font-bold text-gray-700'>{discoveredPerson}</span></p>
+                        <p>Discoverd By:  <span className='font-bold text-gray-700 '>{discoveredPerson}</span></p>
                     </div>
                     <div className='flex items-center gap-3 '>
                         <FcEditImage></FcEditImage>
--- src/pages/MyArtifacts.jsx
+++ src/pages/MyArtifacts.jsx
@@ -1,26 +1,28 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import MyAddedArtifacts from '../components/MyAddedArtifacts';
+import { AuthContext } from '../AuthProvider/AuthProvider';
 
 const MyArtifacts = () => {
 
     const [art, setArt] = useState([]);
+    const { user } = useContext(AuthContext);
 
     useEffect(() => {
-        fetch(`http://localhost:4000/histories?email=john@john.ed`)
+        fetch(`http://localhost:4000/histories?email=${user?.email}`)
             .then(res => res.json())
             .then(data => {
                 setArt(data);
             })
-    }, [])
+    }, [user?.email])
     return (
         <div className='my-14'>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 w-11/12 mx-auto'>
                 {
                     art.length === 0
                         ?
                         <p className='text-2xl text-red-400 font-black'>You Not Added Artifacts</p>
                         :
-                        art?.map(singleArt => <MyAddedArtifacts key={singleArt._id} singleArt={singleArt}></MyAddedArtifacts>)
+                        art?.map(singleArt => <MyAddedArtifacts key={singleArt._id} singleArt={singleArt} setArt={setArt} art={art}></MyAddedArtifacts>)
                 }
             </div>
         </div>
