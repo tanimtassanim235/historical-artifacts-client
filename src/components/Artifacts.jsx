92dbde31e3745f332e541545c7f72e19e824cd0f
Tanim Tassanim <tanimtassanim235@gmail.com>
2024-12-27 21:08:57 +0600

    seach  Functionality added in all artifacts page

--- src/components/Artifacts.jsx
+++ src/components/Artifacts.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Artifacts = ({ art }) => {
-    console.log(art);
     const { _id, name, image, currentLocation, artifactsType, context } = art
     return (
         <div>
--- src/pages/Allartifacts.jsx
+++ src/pages/Allartifacts.jsx
@@ -4,14 +4,28 @@ import Artifacts from '../components/Artifacts';
 const Allartifacts = () => {
 
     const [artifacts, setArtifacts] = useState([])
+    const [search, setSearch] = useState("")
+
+    console.log(search);
 
     useEffect(() => {
         fetch('http://localhost:4000/histories')
             .then(res => res.json())
             .then(data => setArtifacts(data))
     }, [])
+
+    useEffect(() => {
+        fetch(`http://localhost:4000/histories?searchParams=${search}`)
+            .then(res => res.json())
+            .then(data => setArtifacts(data))
+    }, [search])
     return (
-        <div className='py-16'>
+        <div className='py-16 w-11/12 mx-auto'>
+            <input
+                type="text"
+                placeholder="Search here"
+                onChange={(e) => setSearch(e.target.value)}
+                className="input input-bordered input-error w-full max-w-5xl mx-auto mb-14" />
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
                 {
                     artifacts.map(arti => <Artifacts key={arti._id} art={arti}></Artifacts>)
